02b05a350cbe6cd5fe0d754abc13967204ab5ce5
Koffivi V. Woglo <59232299+VincentWoglo@users.noreply.github.com>
2022-03-28 19:25:33 -0600

    Add files via upload

--- Script/Animation.js
+++ Script/Animation.js
@@ -4,3 +4,19 @@ gsap.from(Restaurant, {
     duration: 1,
     y:-50
 })
+
+
+MenuItem = document.getElementsByClassName("MenuItem")
+gsap.from(MenuItem, {
+  opacity: 0,
+    duration: 1,
+    y:-50,
+    stagger: 0.2,
+    delay: 0.1,
+    ease: Power2.easeOut
+})
+
+gsap.from(Restaurant, {
+  scrollTrigger: "body", // start the animation when ".box" enters the viewport (once)
+  y: 0
+});
